ed44f3a345974518bd5dc0e57e49f84369c0e4f7
Dev Shah <devshah2401@gmail.com>
2022-07-26 11:56:22 +0530

    working react api call -1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
+// function refreshPage() {
+//   window.location.reload(false);
+// }
 function App() {
+  const url = "https://catfact.ninja/fact";
+
+  const [isLoadingData, setisLoadingData] = React.useState(false);
+
+  const [name, setName] = useState("");
+  function callonClick() {
+    fetch(url)
+      .then((response) =>
+        response != null ? response.json() : "Check your network"
+      )
+      .then((r)=> {
+        setName(r);
+        setisLoadingData(false);
+      } );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        {
+        !isLoadingData ?
+        ( 
+        <p> {name.fact + " " + name.length}</p>) : (<p>'Please wait'</p>)
+        }
+      <button onClick={callonClick}>Click</button>
     </div>
   );
+
+  
 }
 
 export default App;
